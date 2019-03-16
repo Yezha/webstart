bdac59272d705e335a35fc886b9ea92142b9ac28
Yerzhan <43063442+Yezha@users.noreply.github.com>
2019-03-16 22:23:02 +0300

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
 var button = document.querySelector('#button');
-var modal = document.querySelector('#modal')
+var modal = document.querySelector('#modal');
+var close = document.querySelector('#close')
 
 button.addEventListener('click', function() {
    modal.classList.add('modal_active');
 });
 
-console.log('Hello, world');
+close.addEventListener('click', function() {
+  modal.classList.remove('modal_active');
+});
